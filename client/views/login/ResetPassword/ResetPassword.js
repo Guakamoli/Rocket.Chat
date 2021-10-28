e9642b3ce910aa912311bcf20d044afec233d515
flxxyz <i@mdzz.name>
2021-10-28 18:18:30 +0800

    Update ResetPassword.js

--- client/views/login/ResetPassword/ResetPassword.js
+++ client/views/login/ResetPassword/ResetPassword.js
@@ -3,7 +3,7 @@ import { useSafely } from '@rocket.chat/fuselage-hooks';
 import React, { useState, useCallback, useMemo } from 'react';
 import toastr from 'toastr';
 
-import { useRouteParameter } from '../../../contexts/RouterContext';
+import { useRouteParameter, useRoute } from '../../../contexts/RouterContext';
 import { useMethod } from '../../../contexts/ServerContext';
 import { useTranslation } from '../../../contexts/TranslationContext';
 import { useUser } from '../../../contexts/UserContext';
@@ -36,7 +36,7 @@ const ResetPassword = () => {
 		params,
 	);
 
-	// const router = useRoute('home');
+	const router = useRoute('home');
 
 	const changePasswordReason = getChangePasswordReason(user || {});
 
@@ -73,7 +73,16 @@ const ResetPassword = () => {
 				setIsLoading(false);
 			}
 		},
-		[isSubmitDisabled, setIsLoading, token, resetPassword, newPassword, setUserPassword, setError],
+		[
+			isSubmitDisabled,
+			setIsLoading,
+			token,
+			resetPassword,
+			newPassword,
+			router,
+			setUserPassword,
+			setError,
+		],
 	);
 
 	return (
