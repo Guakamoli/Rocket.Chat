8e7d4c1dda4425d3c80499b86a8b39426f1b3f32
flxxyz <i@mdzz.name>
2021-12-03 11:45:27 +0800

    Chore: 添加苹果universal links

--- imports/kameo/server/routers/thirdpartyVerify.js
+++ imports/kameo/server/routers/thirdpartyVerify.js
@@ -12,3 +12,25 @@ WebApp.connectHandlers.use('/kdGu9D1xAS.txt', Meteor.bindEnvironment(function(re
 	res.write('17a3dcf4d4c56546d2a63b08c1e94ded');
 	res.end();
 }));
+
+WebApp.connectHandlers.use('/.well-known/apple-app-site-association', Meteor.bindEnvironment(function(req, res/* , next*/) {
+	const data = {
+		applinks: {
+			apps: [],
+			details: [
+				{
+					appID: 'K6X9285QVU.co.goduck.goducktalent.ios',
+					paths: ['NOT /login', '*'],
+				},
+				{
+					appID: 'K6X9285QVU.co.goduck.goduck.ios',
+					paths: ['/login'],
+				},
+			],
+		},
+	};
+
+	res.writeHead(200, { 'content-type': 'application/json' });
+	res.write(JSON.stringify(data));
+	res.end();
+}));
