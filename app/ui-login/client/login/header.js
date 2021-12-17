fb64dd710835e72a847460142a632356cf003474
shijie <a66377172@qq.com>
2021-12-17 16:04:39 +0800

    修改logo

--- app/ui-login/client/login/header.js
+++ app/ui-login/client/login/header.js
@@ -4,13 +4,17 @@ import { Template } from 'meteor/templating';
 import { settings } from '../../../settings';
 
 Template.loginHeader.helpers({
-	logoUrl() {
-		const asset = settings.get('Assets_logo');
-		const productCode = Meteor.settings.public.PRODUCT_CODE;
-		const imageUrl = productCode === 'GODUCK' ? 'images/logo/torimi.png' : 'images/logo/paiya.png';
-		const prefix = __meteor_runtime_config__.ROOT_URL_PATH_PREFIX || '';
-		if (asset != null) {
-			return `${ prefix }/${ asset.url || imageUrl }`;
-		}
-	},
+    productCodeWidth(){
+        return Meteor.settings.public.PRODUCT_CODE === 'GODUCK' ? '' : '125px';
+    },
+    logoUrl() {
+        const asset = settings.get('Assets_logo');
+        const productCode = Meteor.settings.public.PRODUCT_CODE;
+        console.log(productCode)
+        const imageUrl = productCode === 'GODUCK' ? 'images/logo/torimi.png' : 'images/logo/paiya.png';
+        const prefix = __meteor_runtime_config__.ROOT_URL_PATH_PREFIX || '';
+        if (asset != null) {
+            return `${ prefix }/${ asset.url || imageUrl }`;
+        }
+    },
 });
