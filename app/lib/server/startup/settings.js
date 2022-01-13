a66be9def61d0c41695c57ab054fb02d3e060a82
kezhiyu <kzyman@users.noreply.github.com>
2022-01-13 16:33:14 +0800

    修正设置

--- app/lib/server/startup/settings.js
+++ app/lib/server/startup/settings.js
@@ -1550,6 +1550,10 @@ settings.addGroup('Layout', function() {
 			type: 'boolean',
 			public: true,
 		});
+		this.add('UI_Use_Default_Avatar', '', {
+			type: 'string',
+			public: true,
+		});
 		this.add('Accounts_Direct_Save_Avatar_Url', true, {
 			type: 'boolean',
 			public: true,
