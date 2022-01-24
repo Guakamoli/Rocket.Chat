6f0ba38f4c3fa4b28167b6aed5dc2a6659cd8c65
flxxyz <i@mdzz.name>
2022-01-24 11:04:10 +0800

    [FEAT]: 基于消息的推送功能

--- imports/kameo/server/methods/postMessages.js
+++ imports/kameo/server/methods/postMessages.js
@@ -1,7 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
-import { Messages } from '../../../../app/models';
+import { Messages, Rooms } from '../../../../app/models';
+import { sendMessage } from '../../../../app/lib/server/functions';
 
 Meteor.methods({
 	kameoPostMessages(messages) {
@@ -15,5 +16,21 @@ Meteor.methods({
 			}
 			return msg;
 		});
+	},
+	kameoBotForwardMessage(message, sender, receiverId) {
+		const room = Meteor.runAsUser(receiverId, function() {
+			const { rid } = Meteor.call('createDirectMessage', 'rocket.cat');
+			return Rooms.findOneById(rid);
+		});
+
+		if (message.metadata.rid && message.metadata.category !== 'reaction') {
+			const firstDiscussionMessage = Messages.findOne({ rid: message.metadata.rid }, { sort: { ts: 1 } });
+			message.metadata.messageId = firstDiscussionMessage._id;
+			message.metadata.prid = firstDiscussionMessage.prid;
+			message.metadata.drid = firstDiscussionMessage.drid;
+			message.attachments = firstDiscussionMessage.attachments || [];
+		}
+
+		Promise.await(sendMessage(sender, message, room, false));
 	},
 });
