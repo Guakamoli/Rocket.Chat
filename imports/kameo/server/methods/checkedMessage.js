04276790190e3ededa2f6e23f178c5c200ee27fe
lyihuan <15170674326@163.com>
2022-03-21 14:43:50 +0800

    feat: 新增message过滤method

--- imports/kameo/server/methods/checkedMessage.js
+++ imports/kameo/server/methods/checkedMessage.js
@@ -2,6 +2,17 @@ import { Meteor } from 'meteor/meteor';
 
 import { settings } from '../../../../app/settings/server';
 
+const badWordsList = settings.get('Message_BadWordsFilterList');
+const whiteList = settings.get('Message_BadWordsWhitelist');
+
+const words = badWordsList?.split(',').map((word) => word.trim()).filter(Boolean) || [];
+
+if (whiteList?.length) {
+	const whiteWords = whiteList?.split(',').map((word) => word.trim()).filter(Boolean) || [];
+	const beforeWords = words.filter((word) => !whiteWords.includes(word));
+	words.splice(0).push(...beforeWords);
+}
+
 Meteor.methods({
 	kameoCheckedMessage(message) {
 		const allowBadWordsFilter = settings.get('Message_AllowBadWordsFilter');
@@ -9,18 +20,7 @@ Meteor.methods({
 			return;
 		}
 
-		const badWordsList = settings.get('Message_BadWordsFilterList');
-		const whiteList = settings.get('Message_BadWordsWhitelist');
-
-		const words = badWordsList?.split(',').map((word) => word.trim()).filter(Boolean) || [];
-
-		if (whiteList?.length) {
-			const whiteWords = whiteList?.split(',').map((word) => word.trim()).filter(Boolean) || [];
-			const beforeWords = words.filter((word) => !whiteWords.includes(word));
-			words.splice(0).push(...beforeWords);
-		}
-
-		if (words.some((word) => message.msg.includes(word))) {
+		if (words.some((word) => (message || '').includes(word))) {
 			throw new Meteor.Error('filter-bad-words-failed', 'Filter Bad words Failed');
 		}
 	},
--- imports/kameo/server/methods/filterBadWords.ts
+++ imports/kameo/server/methods/filterBadWords.ts
@@ -12,34 +12,15 @@ Meteor.startup(() => {
 	});
 	Tracker.autorun(() => {
 		Dep.depend();
-		const allowBadWordsFilter = settings.get('Message_AllowBadWordsFilter');
 
 		callbacks.remove('beforeSaveMessage', 'filterBadWords');
-
-		if (!allowBadWordsFilter) {
-			return;
-		}
-
-		const badWordsList = settings.get('Message_BadWordsFilterList') as string | undefined;
-		const whiteList = settings.get('Message_BadWordsWhitelist') as string | undefined;
-
-		const words = badWordsList?.split(',').map((word) => word.trim()).filter(Boolean) || [];
-
-		if (whiteList?.length) {
-			const whiteWords = whiteList?.split(',').map((word) => word.trim()).filter(Boolean) || [];
-			const beforeWords = words.filter((word) => !whiteWords.includes(word));
-			words.splice(0).push(...beforeWords);
-		}
 
 		callbacks.add('beforeSaveMessage', function(message: IMessage) {
 			if (!message.msg) {
 				return message;
 			}
 
-			if (words.some((word) => message.msg.includes(word))) {
-				throw new Meteor.Error('filter-bad-words-failed', 'Filter Bad words Failed');
-			}
-
+			Meteor.call('kameoCheckedMessage', message.msg);
 			return message;
 		}, callbacks.priority.HIGH, 'filterBadWords');
 	});
