5c3efb9938933db7859a25734791560e271e7c00
flxxyz <i@mdzz.name>
2022-03-24 16:44:01 +0800

    FIX: 限制同步消息的类型，并且为审核通过

--- app/lib/server/methods/getChannelHistory.js
+++ app/lib/server/methods/getChannelHistory.js
@@ -9,7 +9,7 @@ import { normalizeMessagesForUser } from '../../../utils/server/lib/normalizeMes
 import { getHiddenSystemMessages } from '../lib/getHiddenSystemMessages';
 
 Meteor.methods({
-	getChannelHistory({ rid, latest, oldest, inclusive, offset = 0, count = 20, unreads, showThreadMessages = true }) {
+	getChannelHistory({ rid, latest, oldest, inclusive, offset = 0, count = 20, unreads, showThreadMessages = true, filters = {} }) {
 		check(rid, String);
 
 		if (!Meteor.userId()) {
@@ -52,8 +52,8 @@ Meteor.methods({
 		}
 
 		const records = _.isUndefined(oldest)
-			? Messages.findVisibleByRoomIdBeforeTimestampNotContainingTypes(rid, latest, hiddenMessageTypes, options, showThreadMessages, inclusive).fetch()
-			: Messages.findVisibleByRoomIdBetweenTimestampsNotContainingTypes(rid, oldest, latest, hiddenMessageTypes, options, showThreadMessages, inclusive).fetch();
+			? Messages.findVisibleByRoomIdBeforeTimestampNotContainingTypes(rid, latest, hiddenMessageTypes, options, showThreadMessages, inclusive, filters).fetch()
+			: Messages.findVisibleByRoomIdBetweenTimestampsNotContainingTypes(rid, oldest, latest, hiddenMessageTypes, options, showThreadMessages, inclusive, filters).fetch();
 
 		const messages = normalizeMessagesForUser(records, fromUserId);
 
@@ -76,6 +76,8 @@ Meteor.methods({
 							},
 						},
 						showThreadMessages,
+						false,
+						filters,
 					);
 
 					firstUnread = unreadMessages.fetch()[0];
