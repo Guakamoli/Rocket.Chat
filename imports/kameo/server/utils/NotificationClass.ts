053107dc380635b2faf6e8d2961ab772ea9f01e7
lyihuan <40694318+lyihuan@users.noreply.github.com>
2022-04-08 10:25:28 +0800

    fix: 发post推送,story不发推送 (#413)
    * fix: 测试发post推送
    
    * fix: 发post推送,story不发推送

--- imports/kameo/server/utils/NotificationClass.ts
+++ imports/kameo/server/utils/NotificationClass.ts
@@ -104,14 +104,15 @@ export default class NotificationClass {
 	}
 
 	push({ uid }: INotification, item: INotificationItemPush): void {
-		const { roomName, username, message, payload, badge = 1, category } = item.data;
+		const { roomName, username, message: originalMessage, payload, badge = 1, category } = item.data;
 
-		if (uid === 'rocket.cat' || payload.messageType !== 'activity') {
+		if (uid === 'rocket.cat' || !['activity', 'post'].includes(payload.messageType || '')) {
 			return;
 		}
 
 		const idOnly = settings.get('Push_request_content_from_server');
 		const title = idOnly ? '' : payload.sender.name || username || roomName;
+		const message = originalMessage.replace('%s', title);
 
 		const iOSAppKey = currentProduct({
 			PAIYA: Number(process.env.MPUSH_APPKEY_PAIYA_IOS),
