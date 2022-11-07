5837c7057e6f3fbd746597d809c52e477fc6b5b2
lyihuan <15170674326@163.com>
2022-11-07 13:16:02 +0800

    fix: 主频道新增角色权限判断逻辑

--- app/lib/server/methods/sendMessage.js
+++ app/lib/server/methods/sendMessage.js
@@ -57,6 +57,7 @@ export function executeSendMessage(uid, message) {
 			username: 1,
 			type: 1,
 			name: 1,
+			roles: 1,
 		},
 	});
 	let { rid } = message;
@@ -76,6 +77,10 @@ export function executeSendMessage(uid, message) {
 		const room = canSendMessage(rid, { uid, username: user.username, type: user.type });
 		if (room.individualMain && room?.u?._id && uid !== room?.u?._id) {
 			throw new Error('You can\'t send messages because the room is readonly.');
+		}
+
+		if (room.individualMain && Array.isArray(user?.roles) && !user.roles.includes('creator') && user._id === room?.u?._id) {
+			throw new Error('No creator permission.');
 		}
 		metrics.messagesSent.inc(); // TODO This line needs to be moved to it's proper place. See the comments on: https://github.com/RocketChat/Rocket.Chat/pull/5736
 		return sendMessage(user, message, room, false);
