bc837161cf52386ee4fdf77ce54de34941c1d376
flxxyz <i@mdzz.name>
2022-12-23 18:22:09 +0800

    chore: 广播到注册的所有实例

--- imports/kameo/server/functions/revo.ts
+++ imports/kameo/server/functions/revo.ts
@@ -13,5 +13,5 @@ type Event = {
 
 export function notify(event: Event): void {
 	const { userId, eventName, eventData } = event;
-	notifications.notifyUserInThisInstance(userId, eventName, eventData);
+	notifications.notifyUser(userId, eventName, eventData);
 }
